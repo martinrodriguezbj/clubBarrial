0f04160f7710cf44055be9acc13ce8fe860c143f
Martin Ezequiel Rodriguez <martinrodrig2001@gmail.com>
2023-02-01 17:16:22 -0300

    realizo commit

---
+++ portal/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <Home />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Home from "@/components/Home.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style>
+.home {
+  width: 100%;
+  height: 100vh;
+  background-color: #e2dfdf;
+  font-family: "Share Tech", sans-serif;
+  position: relative;
+}
+</style>
